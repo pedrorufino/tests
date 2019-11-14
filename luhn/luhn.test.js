6ed1659ab036944584fe286604eb50bcdeebb1e6
pedro <opedrolps@gmail.com>
2019-11-13 22:37:08 -0300

    add luhn algorithm

--- luhn/luhn.test.js
+++ luhn/luhn.test.js
@@ -8,14 +8,14 @@ test('Dobrar o valor, se o produto tiver digitos, separa-los e soma-los', () => 
   expect(f.sumDoubbleDigit(22)).toBe(8)
 })
 
-test('Dobrar o valor', () => {
+test('Testar número de cartão válido', () => {
   expect(f.luhnAlgorithm('4000 1234 5678 9314')).toBeTruthy()
 })
 
-test('Dobrar o valor', () => {
+test('Testar número de cartão inválido', () => {
   expect(f.luhnAlgorithm('4000 1234 5678 9010')).toBeFalsy()
 })
 
-test('Dobrar o valor', () => {
+test('Testar número de cartão válido sem a normalização correta', () => {
   expect(f.luhnAlgorithm('4000.1234.5678.931-4')).toBeTruthy()
 })
